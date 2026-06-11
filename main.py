f39f420891655827b4e25c6448984196216e7843
BulentEmreMollamehmetoglu <106030451+BulentEmreMollamehmetoglu@users.noreply.github.com>
2026-06-11 21:24:59 +0300

    Zero to Hero

---
+++ main.py
@@ -0,0 +1,225 @@
+'''
+arr = [1, 2, 2, 3, 4, 4, 4, 5, 5]
+return_dict = {}
+
+for i in range(len(arr)):
+    if arr[i] not in return_dict:
+        return_dict[arr[i]] = 1
+    else:
+        continue
+
+#print(list(return_dict))
+
+
+###########################
+arr2 = [2, 2, 2, 2, 2]
+hash_set = set()
+
+for i in range(len(arr2)):
+    if arr2[i] not in hash_set:
+        hash_set.add(arr2[i])
+#print(list(hash_set))
+
+
+#########################
+
+# Two pointers
+arr3 = [1, 2, 2, 3, 4, 4, 4, 5, 5]
+
+write = 1
+
+for read in range(1,len(arr3)):
+    if arr3[read] != arr3[read-1]:
+        #print(f"arr read {arr3[read]} and arr write {arr3[write]}")
+        arr3[write] = arr3[read]
+        #print(f"arr {arr3}")
+        write += 1
+
+
+#print(arr3[:write])
+
+
+
+nums = [-4,-1,0,-3,10]
+
+left = 0
+right = len(nums) - 1
+result_arr5 = []
+
+while left <= right:
+    if abs(nums[left]) > abs(nums[right]):
+        result_arr5.append(nums[left] ** 2)
+        left += 1
+    else:
+        result_arr5.append(nums[right] ** 2)
+        right -= 1
+result_arr5.reverse()
+#print(result_arr5)
+
+'''
+
+'''
+Generating All Subarrays
+
+Given an array arr[], the task is to generate all the possible subarrays of the given array.
+arr = [1, 2,3,4]
+return_arr = []
+
+
+for i in range(len(arr)):
+        for j in range(i+1,len(arr)+1):
+                if arr[i:j] == []:
+                        continue
+                return_arr.append(arr[i:j])
+
+print(return_arr)
+'''
+
+'''
+Reverse an array arr[]. Reversing an array means 
+rearranging the elements such that the first element becomes the last, the second element becomes second last and so on.
+
+
+
+arr = [4,5,1,2] 
+return_arr = []
+
+for i in range(len(arr)-1,-1,-1):
+    return_arr.append(arr[i])
+
+#print(return_arr)
+
+
+#Time complexity O(n) Space complexity O(n)
+
+#Second approach
+# Two pointers maybe
+
+left = 0
+right = len(arr) - 1
+temp = 0
+while left < right:
+    arr[left], arr[right] = arr[right], arr[left]
+    left += 1
+    right -= 1
+print(f"after {arr}")
+'''
+# Time Complexity O(n/2) -> O(n)  / Space Complexity O(1)
+
+
+# Rotate an Array - Clockwise or Right
+
+#Naive approach
+'''
+arr = [1,2,3,4,5,6]
+
+for _ in range(3):
+    last = arr[len(arr)-1]
+    for i in range(len(arr)-1,0,-1):
+        arr[i] = arr[i-1]
+    arr[0] = last
+
+#print(f"after {arr}")
+'''
+#Using Temporary arr
+'''
+arr = [1, 2, 3, 4, 5, 6]
+d = 2
+d %= len(arr)
+print(arr)
+
+arr[0:d] = arr[-d:]
+arr[d:] = arr[:-d]
+print(arr)
+'''
+#arr = [1, 2, 3, 4, 5, 6]
+#print(arr[-2:]) # get this element by starting that position which is -2 ->  0  1  2  3  4  5
+      #                                                                   -> -6 -5 -4 -3 -2 -1 
+#print(arr[:-2]) # get until this element based on the current index which is given
+#print(arr[4:-1])
+
+arr = [1, 2, 3, 4, 5, 6]
+d = 2
+return_arr = [0] * len(arr)
+for i in range(len(arr)-1,-1,-1):
+    return_arr[(i +d) % len(arr)] = arr[i]
+
+#print(return_arr)
+
+# Time O(n) Space O(n)
+'''
+arr2 = [1, 2, 3, 4, 5, 6]
+k = 2
+k = k % len(arr2)
+l, r = 0, len(arr2) - 1
+print(arr2)
+while l < r:
+    arr2[l], arr2[r] = arr2[r], arr2[l]
+    l, r = l+1 , r-1 
+
+# [6,5,4,3,2,1]
+l, r = 0, k - 1
+while l < r:
+    arr2[l], arr2[r] = arr2[r], arr2[l]
+    l, r = l+1 , r-1 
+
+# [5,6,4,3,2,1]
+
+l, r = k, len(arr2) - 1
+while l < r:
+    arr2[l], arr2[r] = arr2[r], arr2[l]
+    l, r = l+1 , r-1 
+ 
+print(arr2)
+'''
+
+'''
+hash_set = set()
+hash_set.add(2)
+hash_set.add(2)
+hash_set.add(2)
+hash_set.add(2)
+
+print(hash_set)
+'''
+class Solution:
+        
+    def isSubset(self, a : list[int],b : list[int]) -> bool:
+        
+        hash_set = set(a)
+        for num in b:
+            if num not in hash_set:
+                return False
+        return True
+
+    def isAnagram(self, s: str, t: str) -> bool:
+        '''
+        Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+
+        An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+        '''
+        hash_map_s = {}
+        hash_map_t = {}
+
+        if len(s) != len(t):
+            return False
+        for i in s:
+            if i not in hash_map_s:
+                hash_map_s[i] = 1
+            else:
+                hash_map_s[i] = hash_map_s.get(i) + 1
+
+        for j in t:
+            if j not in hash_map_t:
+                hash_map_t[j] = 1
+            else:
+                hash_map_t[j] = hash_map_t.get(j) + 1
+
+        return True if hash_map_s == hash_map_t else False
+def main():
+
+    solution_Ex = Solution()
+    #print(solution_Ex.isSubset(a,b))
+    print(solution_Ex.isAnagram(s = "jar", t = "jam"))
+
+main()
