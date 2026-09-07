c17b5abc4cb47629a3af88fa75e689a44b904499
BulentEmreMollamehmetoglu <106030451+BulentEmreMollamehmetoglu@users.noreply.github.com>
2026-09-07 07:13:42 +0300

    BST Iterator

--- main.py
+++ main.py
@@ -2079,7 +2079,7 @@ class ListNode:
                 curr = curr.left
             curr = stack.pop()
             curr = curr.right
-        # Time and space : O(n)
+        # Time and space : O(n) where n is the height of the tree
 
 
     def iterativeInorderDFS(self,root : TreeNode):
@@ -2112,6 +2112,32 @@ class TreeNode :
         self.val = val
         self.left = None
         self.right = None
+
+
+
+class BSTIterator:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.stack = []
+        self.dfs(root)
+    def next(self) -> int:
+        curr = self.stack.pop()
+        if curr.right:
+            self.dfs(curr.right)
+        return curr.val
+    def hasNext(self) -> bool:
+        return True if self.stack else False
+    def dfs(self,root):
+        curr = root
+        while curr:
+            self.stack.append(curr)
+            curr = curr.left
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
+
 def main():  
 
     solution_Ex = Solution()
